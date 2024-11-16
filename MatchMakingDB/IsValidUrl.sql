c48561ef7e2ce58a47a260b020b91d6aa79bae7e
Computer Virus <86645415+computer-virus@users.noreply.github.com>
2024-11-16 12:47:32 -0700

    Added Regex for url validation as a comment.

--- MatchMakingDB/IsValidUrl.sql
+++ MatchMakingDB/IsValidUrl.sql
@@ -1,5 +1,8 @@
 ﻿Create Function [dbo].[IsValidUrl] (@url varchar(2048))
 Returns bit As
 Begin
+	-- Regex For Url: ^[(http(s)?:\/\/)?(www\.)?a-zA-Z0-9@:%\._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+\.~#?&\/=]*)$
+
+	-- To be replace with real logic
 	Return 1
 End

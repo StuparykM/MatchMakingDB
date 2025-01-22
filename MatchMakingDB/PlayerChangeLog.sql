c569f2ac181024768c0541866bfba4443c5484f2
Mike Stuparyk <stuparykmike@gmail.com>
2025-01-22 12:13:42 -0700

    Prepped the playerchangelog for triggers and procedures

--- MatchMakingDB/PlayerChangeLog.sql
+++ MatchMakingDB/PlayerChangeLog.sql
@@ -4,27 +4,42 @@
 		constraint PK_PlayerChangeLog primary key clustered not null,
 	UnixID int
 		constraint FK_PlayerChangeLog_UnixID references Player(PlayerUnixID) not null,
-		FirstName varchar(50) null
-		Constraint CK_PlayerChangeLog_FirstName Check (FirstName like Trim(FirstName)),
-	LastName varchar(50) null
-		Constraint CK_PlayerChangeLog_LastName Check (LastName like Trim(LastName)),
-	FullName As FirstName + ' ' + LastName,
-	Wins int null
-		Constraint DF_PlayerChangeLog_Wins Default 0
-		Constraint CK_PlayerChangeLog_Wins Check (Wins >= 0),
-	Losses int null
-		Constraint DF_PlayerChangeLog_Losses Default 0
-		Constraint CK_PlayerChangeLog_Losses Check (Losses >= 0),
-	RankingScore int null
-		Constraint DF_PlayerChangeLog_RankingScore Default 1000
-		Constraint CK_PlayerChangeLog_RankingScore Check (RankingScore >= 0),
+	OldFirstName varchar(50) null
+		Constraint CK_PlayerChangeLog_OldFirstName Check (OldFirstName like Trim(OldFirstName)),
+	NewFirstName varchar(50) null
+		Constraint CK_PlayerChangeLog_NewFirstName Check (NewFirstName like Trim(NewFirstName)),
+	OldLastName varchar(50) null
+		Constraint CK_PlayerChangeLog_OldLastName Check (OldLastName like Trim(OldLastName)),
+	NewLastName varchar(50) null
+		Constraint CK_PlayerChangeLog_NewLastName Check (NewLastName like Trim(NewLastName)),
+	OldFullName As OldFirstName + ' ' + OldLastName,
+	NewFullName As NewFirstName + ' ' + NewLastName,
+	OldWins int null
+		Constraint DF_PlayerChangeLog_OldWins Default 0
+		Constraint CK_PlayerChangeLog_OldWins Check (OldWins >= 0),
+	NewWins int null
+		Constraint DF_PlayerChangeLog_NewWins Default 0
+		Constraint CK_PlayerChangeLog_NewWins Check (NewWins >= 0),
+	OldLosses int null
+		Constraint DF_PlayerChangeLog_OldLosses Default 0
+		Constraint CK_PlayerChangeLog_OldLosses Check (OldLosses >= 0),
+	NewLosses int null
+		Constraint DF_PlayerChangeLog_NewLosses Default 0
+		Constraint CK_PlayerChangeLog_NewLosses Check (NewLosses >= 0),
+	OldRankingScore int null
+		Constraint DF_PlayerChangeLog_OldRankingScore Default 1000
+		Constraint CK_PlayerChangeLog_OldRankingScore Check (OldRankingScore >= 0),
+	NewRankingScore int null
+		Constraint DF_PlayerChangeLog_NewRankingScore Default 1000
+		Constraint CK_PlayerChangeLog_NewRankingScore Check (NewRankingScore >= 0),
 	CreationDate DateTime null
 		Constraint DF_PlayerChangeLog_CreationDate Default GetDate()
 		Constraint CK_PlayerChangeLog_CreationDate Check (CreationDate <= GetDate()),
 	ChangeDate datetime not null,
-	AdminChange int null
+	AdminID int null
 		Constraint FK_PlayerChangeLog_AdminID foreign key 
 			references Admin(AdminID)
+
 )
 GO
 

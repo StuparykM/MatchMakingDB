4e6996dc8f210a6695c86ce801864ac0339e3080
StuparykM <stuparykmike@gmail.com>
2025-03-05 10:41:47 -0700

    Created Challenge table

---
+++ MatchMakingDB/Challenge.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Challenge]
+(
+	ChallengeID int identity(1,1) 
+		constraint PK_Challenge primary key clustered not null,
+	Challenger int not null
+		Constraint FK_Challenge_Challenger references Player(PlayerUnixID),
+	Opponent int not null
+		Constraint FK_Challenge_Opponent references Player(PlayerUnixID),
+	ChallengeDate DateTime not null
+		constraint CK_Challenge_Date CHECK (ChallengeDate <= GETDATE()),
+	IsAccepted bit not null
+)

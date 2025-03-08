4bd1f619505ad9f8e3f09affd3d54b4f40851722
StuparykM <stuparykmike@gmail.com>
2025-03-08 10:44:07 -0700

    More work on challenge table

--- MatchMakingDB/Challenge.sql
+++ MatchMakingDB/Challenge.sql
@@ -21,4 +21,78 @@ create nonclustered index IX_Challenge_Opponent
 	ON Challenge(Opponent)
 GO
 
+create trigger TR_Challenge_PreventPKUpdate
+	on "Challenge"
+	For update
+	As
+		Begin
+			if @@ROWCOUNT > 0 and (Update(ChallengeID))
+					Begin
+						rollback transaction
+							raiserror('Cannot change Log ID',16,1)
+					End
+				End
+	Return
+GO
 
+
+create trigger TR_ChallengeChangeLog_AdminCheck
+on Challenge
+for update
+as
+begin
+	DECLARE @PlayerUnixID int;
+	set @PlayerUnixID = Admin.PlayerUnixID;
+	if update(Challenger) or update(Opponent) or update(ChallengeDate)
+	select *
+	from Admin
+	where PlayerUnixID = USER_ID(@PlayerUnixID)/*this function will need to be updated, USER_ID is depreciating in the future*/ 
+if @@ERROR <> 0 
+	begin
+	rollback transaction
+	raiserror('Do not have permissions for update',16,1)
+	end
+end
+GO
+
+create procedure Challenge_InsertData
+(@Challenger int = null, @Opponent int = null, @ChallengeDate datetime = null, @Status varchar(50) = null)
+AS
+	BEGIN TRY
+	BEGIN TRANSACTION
+		IF @Challenger IS NULL
+			BEGIN
+				RAISERROR('Challenger cannot be null',16,1)
+				ROLLBACK TRANSACTION
+				RETURN
+			END
+		IF @Opponent IS NULL
+			BEGIN
+				RAISERROR('Opponent cannot be null',16,1)
+				ROLLBACK TRANSACTION
+				RETURN
+			END
+		IF @ChallengeDate IS NULL
+			BEGIN
+				RAISERROR('Challenge Date cannot be null',16,1)
+				ROLLBACK TRANSACTION
+				RETURN
+			END
+		IF @Status IS NULL
+			BEGIN
+				RAISERROR('Challenge status cannot be null',16,1)
+				ROLLBACK TRANSACTION
+				RETURN
+			END
+		INSERT INTO Challenge (Challenger, Opponent, ChallengeDate, "Status")
+		VALUES (@Challenger, @Opponent, @ChallengeDate, @Status)
+		COMMIT TRANSACTION
+			END TRY
+				BEGIN CATCH
+					IF @@TRANCOUNT > 0
+					ROLLBACK TRANSACTION
+						DECLARE @ErrorMessage NVARCHAR(4000), @ErrorSeverity INT, @ErrorState INT, @ErrorLine INT
+						SELECT @ErrorMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE(), @ErrorLine = ERROR_LINE()
+						RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState, @ErrorLine)
+		END CATCH
+GO

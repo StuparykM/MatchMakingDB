54929d7e276f02cdfe5eee2dac50f953da5aa775
StuparykM <stuparykmike@gmail.com>
2025-03-08 12:04:40 -0700

    Accepted Challenge procedure

--- MatchMakingDB/Challenge.sql
+++ MatchMakingDB/Challenge.sql
@@ -119,3 +119,26 @@ AS
 						RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState, @ErrorLine)
 		END CATCH
 GO
+
+
+create procedure AcceptChallenge
+@ChallengeID int
+AS
+	IF NOT EXISTS (SELECT * from Challenge where ChallengeID = @ChallengeID and "Status" = 'Pending')
+		BEGIN
+			RAISERROR('Challenge does not exist or is still pending',16,1)
+			RETURN
+		END
+	ELSE
+	BEGIN
+		DECLARE @Challenger int, @Opponent int
+		SELECT @Challenger = Challenger, @Opponent = Opponent
+		FROM Challenge where ChallengeID = @ChallengeID
+	UPDATE Challenge
+	SET "Status" = 'Accepted'
+	WHERE ChallengeID = @ChallengeID
+INSERT INTO "Match" (PlayerOne, PlayerTwo)
+VALUES (@Challenger, @Opponent, 'Ongoing')
+END
+
+	

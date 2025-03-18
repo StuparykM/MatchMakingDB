eb86a765103f9163d219fc21655998723918b2c7
StuparykM <stuparykmike@gmail.com>
2025-03-18 12:08:46 -0600

    corrected syntax

--- MatchMakingDB/ChallengeChangeLog.sql
+++ MatchMakingDB/ChallengeChangeLog.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ChallengeChangeLog]
 (
-	ChallengeChangeID int identity (1,1) primary key clustered not null,
+	ChallengeChangeID int identity (1,1) constraint PK_ChallengeChangeLog_ChallengeChangeID primary key clustered not null,
 	OldChallenger int null
 		constraint FK_ChallengeChangeLog_OldChallenger references Player(PlayerUnixID),
 	NewChallenger int null
